b3e6e6602a6568c9f758ae349d1df206115cf159
Andy Tang <andy@provictores.com>
2012-11-30 18:33:40 +0100

    some idea's working out...
    Signed-off-by: Andy Tang <andy@provictores.com>

--- test/spec/transformer.js
+++ test/spec/transformer.js
@@ -1,14 +1,21 @@
-﻿define(['classExtentions'], function () {
+﻿define(['transformer'], function () {
 
 	describe('class transformations', function () {
 
 		function Transformer() {
 			var _foo = 'FOO';
 			var _protected = {
-
+				vehicle : 'car',
+				doAction: function () {
+					throw new Error("Do Action is not implemented");
+				}
 			};
 
-			this.protected = _protected;
+			Transformer.prototype.protected = _protected;
+
+			this.getVehicle = function () {
+				return _protected.vehicle;
+			};
 
 			this.bar = function () {
 				return _foo + ' BAR';
@@ -16,12 +23,47 @@
 		}
 		Transformer.transformable();
 
+		function Optimus() {
+			var _protected = {
+
+			};
+
+			this.protected = _protected;
+
+			this.getVehicle = function () {
+				return _protected.vehicle;
+			};
+		}
+		Transformer.transformsTo(Optimus);
+
 		describe('transformable class', function () {
 			it('should be able to instantiate a transformable a class', function () {
 				var transformer = new Transformer();
 
 				expect(transformer.bar()).toEqual('FOO BAR');
 			});
+
+			it('should not have the _protected in the public anymore', function () {
+				var transformer = new Transformer();
+
+				expect(transformer._protected).toEqual(undefined);
+			});
+
+			it('should still have the protected variables accesable through other functions', function () {
+				var transformer = new Transformer();
+
+				expect(transformer.getVehicle()).toEqual('car');
+			});
+
+		});
+
+		describe('transforming into a class', function () {
+			//it('should be able to transform into an different class', function () {
+			//	var transformer = new Transformer();
+			//	transformer.transformInto(Optimus);
+
+			//	expect(transformer.getVehicle()).toEqual('Truck');
+			//});
 		});
 
 	});
---
+++ transformer.js
@@ -0,0 +1,35 @@
+﻿define('transformer', function () {
+
+	Function.prototype.transformable = function () {
+		createTransformable(this);
+	};
+	
+	function createTransformable(OriginalClass) {
+		var _protected = OriginalClass.prototype._protected;
+
+		function getProtected() {
+			return _protected;
+		}
+
+		function Transformable() {
+			this.transformsInto = function () {
+
+			};
+		}
+
+		OriginalClass.transformsTo = function (Form) {
+
+		};
+
+		function implementTransformable() {
+			OriginalClass.prototype = new Transformable();
+		}
+
+		implementTransformable();
+	}
+
+	function createTransformForm(){
+
+	}
+
+});
