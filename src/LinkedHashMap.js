82ef16f66f60030107451761b85d8d28dc04c7da
Andy Tang <andy.tang@jumio.com>
2014-01-24 11:48:29 +0100

    (#8) Add an entry

---
+++ src/LinkedHashMap.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2014. 
+ *
+ * @Author Andy Tang
+ */
+(function LinkedHashMapScope() {
+    'use strict';
+
+    var Node = Class(function Node() {
+        this.private = {
+            key: null,
+            value: null,
+            previous: null,
+            next: null
+        };
+
+        this.public = {
+            getKey: function getKey() {
+                return this.private.key;
+            },
+            setKey: function setKey(key) {
+                this.private.key = key;
+            },
+            getValue: function getValue() {
+                return this.private.value;
+            },
+            setValue: function setValue(value) {
+                this.private.value = value;
+            },
+            getPrevious: function getPrevious() {
+                return this.private.previous;
+            },
+            setPrevious: function setPrevious(previous) {
+                this.private.previous = previous;
+            },
+            getNext: function getNext() {
+                return this.private.next;
+            },
+            setNext: function setNext(next) {
+                this.private.key = next;
+            }
+        };
+
+        this.constructor = function constructor(key, value, previous, next) {
+            this.private.key = key;
+            this.private.value = value;
+            this.private.previous = previous;
+            this.private.next = next;
+        };
+    });
+
+    var LinkedHashMap = window.LinkedHashMap = Class(function LinkedHashMap() {
+        this.private = {
+            count: 0,
+            hashMap: {}
+        };
+
+        this.public = {
+            add: function add(value) {
+                this.private.hashMap[this.private.count] = new Node(this.private.count, value);
+            }
+        };
+
+        this.constructor = function constructor() {
+
+        };
+    });
+}(window));
---
+++ test/spec/LinkedHashMapSpec.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014. 
+ *
+ * @Author Andy Tang
+ */
+'use strict';
+
+describe('LinkedHashMap', function LinkedHashMapSpec() {
+
+    describe('initalizing an LinkedHashMap', function () {
+        it('should be able to add an entry to the list', function () {
+            var list = new LinkedHashMap();
+            list.add('hello');
+        });
+    });
+});
